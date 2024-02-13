20cffbca968620911ab8addd32dc386c671205d4
felipepalage <fguilherme323@gmail.com>
2024-02-13 16:49:09 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    const weight = parseFloat(document.getElementById('weight').value); // Convertendo para número
+    const height = parseFloat(document.getElementById('height').value); // Convertendo para número
+
+    // Convertendo altura para metros (caso ela esteja em centímetros)
+    const heightInMeters = height > 3 ? height / 100 : height;
+
+    const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+
+    const value = document.getElementById('value');
+    let description = '';
+
+    document.getElementById('infos').classList.remove('hidden');
+
+    if (bmi < 18.5) {
+        description = 'Cuidado! Você está abaixo do peso!';
+    } else if (bmi >= 18.5 && bmi <= 25 ) {
+        description = "Você está no peso ideal!";
+        value.classList.remove('attention');
+        value.classList.add('normal');
+    } else if (bmi > 25 && bmi <= 30 ){
+        description = "Cuidado! Você está com sobrepeso!";
+    } else if (bmi > 30 && bmi <= 35 ) {
+        description = "Cuidado! Você está com obesidade moderada!";
+    } else if (bmi > 35 && bmi <= 40 ) {
+        description = "Cuidado! Você está com obesidade severa!";
+    } else {
+        description = "Cuidado! Você está com obesidade mórbida!";
+    }
+
+    value.textContent = bmi.replace('.', ',');
+    document.getElementById('description').textContent = description;
+});
